69f91de8267d1d804b9c7a96b6fd21a0e8b133be
Chi-Yun Hsu <chsu6@scu.education>
2025-05-07 15:13:39 -0700

    Create 250507.lean

---
+++ LeanStudy/250507.lean
@@ -0,0 +1,23 @@
+import mathlib
+
+def FnUb (f : ℝ → ℝ) (a : ℝ) : Prop :=
+  ∀ x, f x ≤ a
+
+def FnLb (f : ℝ → ℝ) (a : ℝ) : Prop :=
+  ∀ x, a ≤ f x
+
+def FnHasUb (f : ℝ → ℝ) :=
+  ∃ a, FnUb f a
+
+def FnHasLb (f : ℝ → ℝ) :=
+  ∃ a, FnLb f a
+
+variable {f g : ℝ → ℝ}
+
+example {c : ℝ} (ubf : FnHasUb f) (h : c ≥ 0) : FnHasUb fun x ↦ c * f x := by
+  rcases ubf with ⟨a, hfa⟩
+  use c * a
+  intro x
+  rcases hfa x with hfax
+  simp
+  apply mul_le_mul_of_nonneg_left hfax h
