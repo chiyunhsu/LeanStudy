d543af564cafd21b9b8b2185d48f5b3ab9f9fbb8
Chi-Yun Hsu <chiyunhsu@users.noreply.github.com>
2025-06-23 16:57:58 -0700

    Added 250623.lean

---
+++ LeanStudy/250623.lean
@@ -0,0 +1,43 @@
+import Mathlib
+
+def ConvergesTo (s : ℕ → ℝ) (a : ℝ) :=
+  ∀ ε > 0, ∃ N, ∀ n ≥ N, |s n - a| < ε
+
+example : (fun x y : ℝ ↦ (x + y) ^ 2) = fun x y : ℝ ↦ x ^ 2 + 2 * x * y + y ^ 2 := by
+  ext
+  ring
+
+example (a b : ℝ) : |a| = |a - b + b| := by
+  congr
+  ring
+
+example {a : ℝ} (h : 1 < a) : a < a * a := by
+  convert (mul_lt_mul_right _).2 h
+  · rw [one_mul]
+  exact lt_trans zero_lt_one h
+
+set_option linter.unusedVariables false
+theorem convergesTo_const (a : ℝ) : ConvergesTo (fun x : ℕ ↦ a) a := by
+  intro ε εpos
+  use 0
+  intro n nge
+  simp  --rw [sub_self, abs_zero]
+  apply εpos
+
+theorem convergesTo_add {s t : ℕ → ℝ} {a b : ℝ}
+      (cs : ConvergesTo s a) (ct : ConvergesTo t b) :
+    ConvergesTo (fun n ↦ s n + t n) (a + b) := by
+  intro ε εpos
+  dsimp -- this line is not needed but cleans up the goal a bit.
+  have ε2pos : 0 < ε / 2 := by linarith
+  rcases cs (ε / 2) ε2pos with ⟨Ns, hs⟩
+  rcases ct (ε / 2) ε2pos with ⟨Nt, ht⟩
+  use max Ns Nt
+  intro n hn
+  have n_ge_Ns : n ≥ Ns := le_trans (le_max_left Ns Nt) hn
+  have n_ge_Nt : n ≥ Nt := le_trans (le_max_right Ns Nt) hn
+  calc
+    |s n + t n - (a + b)| = |(s n - a) + (t n - b)| := by congr; linarith
+    _ ≤ |s n - a| + |t n - b| := by apply abs_add
+    _ < ε / 2 + ε / 2 := by linarith [hs n n_ge_Ns, ht n n_ge_Nt]
+    _ = ε := by linarith
