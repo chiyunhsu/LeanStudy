8bb752e40fa1534fa0e2946d869f91285d243a44
Chi-Yun Hsu <chiyunhsu@users.noreply.github.com>
2025-07-24 12:01:28 -0700

    Add 4-1 Set.lean

---
+++ LeanStudy/4-1 Sets.lean
@@ -0,0 +1,91 @@
+import Mathlib
+
+/- 4.1 Sets -/
+/- 25/7/24 -/
+
+variable {α : Type*}
+variable (s t u : Set α)
+open Set
+
+-- intersection
+example (h : s ⊆ t) : s ∩ u ⊆ t ∩ u := by
+  rw [subset_def, inter_def, inter_def]
+  rw [subset_def] at h
+  simp only [mem_setOf]
+  rintro x ⟨xs, xu⟩
+  exact ⟨h _ xs, xu⟩
+
+example (h : s ⊆ t) : s ∩ u ⊆ t ∩ u := by
+  simp only [subset_def, mem_inter_iff] at *
+  rintro x ⟨xs, xu⟩
+  exact ⟨h _ xs, xu⟩
+
+example (h : s ⊆ t) : s ∩ u ⊆ t ∩ u := by
+  intro x xsu
+  exact ⟨h xsu.1, xsu.2⟩
+
+example (h : s ⊆ t) : s ∩ u ⊆ t ∩ u :=
+  fun _ ⟨xs, xu⟩ ↦ ⟨h xs, xu⟩
+
+-- union
+example : s ∩ (t ∪ u) ⊆ s ∩ t ∪ s ∩ u := by
+  intro x hx
+  have xs : x ∈ s := hx.1
+  have xtu : x ∈ t ∪ u := hx.2
+  rcases xtu with xt | xu
+  · left
+    show x ∈ s ∩ t
+    exact ⟨xs, xt⟩
+  · right
+    show x ∈ s ∩ u
+    exact ⟨xs, xu⟩
+
+example : s ∩ (t ∪ u) ⊆ s ∩ t ∪ s ∩ u := by
+  rintro x ⟨xs, xt | xu⟩
+  · left; exact ⟨xs, xt⟩
+  · right; exact ⟨xs, xu⟩
+
+example : s ∩ t ∪ s ∩ u ⊆ s ∩ (t ∪ u) := by
+  rintro x (⟨xs, xt⟩ | ⟨xs, xu⟩)
+  · constructor
+    exact xs
+    left; exact xt
+  · constructor
+    exact xs
+    right; exact xu
+
+example : s ∩ t ∪ s ∩ u ⊆ s ∩ (t ∪ u) := by
+  rintro x (⟨xs, xt⟩ | ⟨xs, xu⟩)
+  · exact ⟨xs, Or.inl xt⟩
+  · exact ⟨xs, Or.inr xu⟩
+
+-- set difference
+example : (s \ t) \ u ⊆ s \ (t ∪ u) := by
+  intro x xstu
+  have xs : x ∈ s := xstu.1.1
+  have xnt : x ∉ t := xstu.1.2
+  have xnu : x ∉ u := xstu.2
+  constructor
+  · exact xs
+  intro xtu
+  -- x ∈ t ∨ x ∈ u
+  rcases xtu with xt | xu
+  · show False; exact xnt xt
+  · show False; exact xnu xu
+
+example : (s \ t) \ u ⊆ s \ (t ∪ u) := by
+  rintro x ⟨⟨xs, xnt⟩, xnu⟩
+  use xs
+  rintro (xt | xu) <;> contradiction
+
+example : s \ (t ∪ u) ⊆ (s \ t) \ u := by
+  rintro x ⟨xs, xntu⟩
+  have xnt : x ∉ t := by
+    intro xt
+    apply xntu
+    left; exact xt
+  have xnu : x ∉ u := by
+    intro xu
+    apply xntu
+    right; exact xu
+  exact ⟨⟨xs, xnt⟩, xnu⟩
