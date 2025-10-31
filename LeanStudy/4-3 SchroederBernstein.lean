5efe34d04df0d3ac7c237c35a1dd4b0f2208b649
Chi-Yun Hsu <chiyunhsu@users.noreply.github.com>
2025-10-31 12:33:03 -0700

    Create 4-3 SchroederBernstein.lean

---
+++ LeanStudy/4-3 SchroederBernstein.lean
@@ -0,0 +1,35 @@
+import MathLib
+
+noncomputable section
+open Classical Set Function
+variable {α β : Type*} [Nonempty β]
+
+variable (x : α)
+variable (f : α → β) (g : β → α)
+
+#check (invFun g : α → β)
+#check (leftInverse_invFun : Injective g → LeftInverse (invFun g) g)
+#check (leftInverse_invFun : Injective g → ∀ y, invFun g (g y) = y)
+#check (invFun_eq : (∃ y, g y = x) → g (invFun g x) = x)
+
+def sbAux : ℕ → Set α
+  | 0 => univ \ g '' univ
+  | n + 1 => g '' (f '' sbAux n)
+
+def sbSet :=
+  ⋃ n, sbAux f g n
+
+def sbFun (x : α) : β :=
+  if x ∈ sbSet f g then f x else invFun g x
+
+theorem sb_right_inv {x : α} (hx : x ∉ sbSet f g) : g (invFun g x) = x := by
+  have : x ∈ g '' univ := by
+    contrapose! hx
+    rw [sbSet, mem_iUnion]
+    use 0
+    rw [sbAux, mem_diff]
+    exact ⟨mem_univ x, hx⟩
+  have : ∃ y, g y = x := by
+    rcases this with ⟨y, hy⟩
+    use y, hy.2
+  exact invFun_eq this
