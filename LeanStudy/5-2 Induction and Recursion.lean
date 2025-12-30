a8afe25ea0b6af5efb953845daf0ea825ef4a512
Chi-Yun Hsu <chiyunhsu@users.noreply.github.com>
2025-12-30 13:55:34 -0800

    Create 5-2 Induction and Recursion.lean

---
+++ LeanStudy/5-2 Induction and Recursion.lean
@@ -0,0 +1,60 @@
+import MathLib
+
+#check Nat
+
+example (n : Nat) : n.succ ≠ Nat.zero :=
+  Nat.succ_ne_zero n
+
+example (m n : Nat) (h : m.succ = n.succ) : m = n :=
+  Nat.succ.inj h
+
+def fac : ℕ → ℕ
+  | 0 => 1
+  | n + 1 => (n + 1) * fac n
+
+example : fac 0 = 1 :=
+  rfl
+
+example : fac 0 = 1 := by
+  rw [fac]
+
+example : fac 0 = 1 := by
+  simp [fac]
+
+example (n : ℕ) : fac (n + 1) = (n + 1) * fac n :=
+  rfl
+
+example (n : ℕ) : fac (n + 1) = (n + 1) * fac n := by
+  rw [fac]
+
+example (n : ℕ) : fac (n + 1) = (n + 1) * fac n := by
+  simp [fac]
+
+#check Nat.factorial
+
+theorem fac_pos (n : ℕ) : 0 < fac n := by
+  induction' n with n ih
+  · rw [fac]
+    exact zero_lt_one
+  rw [fac]
+  exact mul_pos n.succ_pos ih
+
+theorem dvd_fac {i n : ℕ} (ipos : 0 < i) (ile : i ≤ n) : i ∣ fac n := by
+  induction' n with n ih
+  · exact absurd ipos (not_lt_of_ge ile)
+  rw [fac]
+  rcases Nat.of_le_succ ile with h | h
+  · apply dvd_mul_of_dvd_right (ih h)
+  rw [h]
+  apply dvd_mul_right
+
+theorem pow_two_le_fac (n : ℕ) : 2 ^ (n - 1) ≤ fac n := by
+  rcases n with _ | n
+  · simp [fac]
+  simp
+  induction' n with n ih
+  · simp [fac]
+  rw [pow_succ', fac]
+  apply Nat.mul_le_mul
+  simp
+  exact ih
